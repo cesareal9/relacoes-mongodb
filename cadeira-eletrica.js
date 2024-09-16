b71f686b3ad4df0f9a64f054df2b1740b13206e3
leachenski <jleachenski@gmail.com>
2024-09-16 19:25:19 -0300

    feat: array de subdocumentos

--- cadeira-eletrica.js
+++ cadeira-eletrica.js
@@ -1,42 +1,44 @@
-import conn from "./conn.js"
+import conn from "./conn.js";
 
-const Schema = conn.Schema
+const Schema = conn.Schema;
 
 const americanoMedioSchema = new Schema({
-    nome: {
-        type: Schema.Types.String,
-        required: true
-    },
-    dataNascimento: {
-        type: Schema.Types.String,
-        required: true
-    }
-})
+  nome: {
+    type: Schema.Types.String,
+    required: true,
+  },
+  dataNascimento: {
+    type: Schema.Types.String,
+    required: true,
+  },
+});
 
 const cadeiraEletrica = new Schema({
-    voltagem: {
-        type: Schema.Types.Number,
-        required: true
-    },
-    americano: americanoMedioSchema
-})
+  voltagem: {
+    type: Schema.Types.Number,
+    required: true,
+  },
+  americano: americanoMedioSchema,
+});
 
-const CadeiraEletrica = conn.model("CadeiraEletrica", cadeiraEletrica)
-
+const CadeiraEletrica = conn.model("CadeiraEletrica", cadeiraEletrica);
 
 const inserir = async () => {
-    try {
-        await CadeiraEletrica.create({
-            americano: {
-                nome: "John Peterson",
-                dataNascimento: "2000/10/05"
-            },
-            voltagem: 380
-        })
-        
-    } catch (error) {
-        console.log(error)
-    }
-}
+  try {
+    const cadeira = await CadeiraEletrica.create({
+      americano: {
+        nome: "John Peterson",
+        dataNascimento: "2000/10/05",
+      },
+      voltagem: 380,
+    });
 
-inserir()
+    console.log(cadeira);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+inserir();
+
+export default CadeiraEletrica;
