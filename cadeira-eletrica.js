08740af2dcd8d62c87fb0fdaaa0351d16b23c3e7
leachenski <jleachenski@gmail.com>
2024-09-16 19:07:50 -0300

    feat: cadeira

---
+++ cadeira-eletrica.js
@@ -0,0 +1,42 @@
+import conn from "./conn.js"
+
+const Schema = conn.Schema
+
+const americanoMedioSchema = new Schema({
+    nome: {
+        type: Schema.Types.String,
+        required: true
+    },
+    dataNascimento: {
+        type: Schema.Types.String,
+        required: true
+    }
+})
+
+const cadeiraEletrica = new Schema({
+    voltagem: {
+        type: Schema.Types.Number,
+        required: true
+    },
+    americano: americanoMedioSchema
+})
+
+const CadeiraEletrica = conn.model("CadeiraEletrica", cadeiraEletrica)
+
+
+const inserir = async () => {
+    try {
+        await CadeiraEletrica.create({
+            americano: {
+                nome: "John Peterson",
+                dataNascimento: "2000/10/05"
+            },
+            voltagem: 380
+        })
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+inserir()
